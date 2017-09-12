c9f35bc96100b8b9c7c1976abc1456aa198577dd
rowan <rowanlupton@gmail.com>
2017-09-12 18:16:12 -0400

    playing with activpy/activitypub stuff

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app, lm
-from flask import Flask, render_template, request, session, flash, redirect, url_for
+from flask import Flask, render_template, request, session, flash, redirect, url_for, jsonify
 from flask_login import login_user, logout_user, login_required, current_user
 from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
@@ -18,13 +18,10 @@ def load_user(handle):
         return None
     return User(u['id'])
 
-def getAllPosts():
-	return mongo.db.posts.find()
-
 @app.route('/')
 @login_required
 def index():
-	posts = getAllPosts()
+	posts = mongo.db.posts.find()
 	return render_template('index.html', posts=posts, mongo=mongo)
 
 
@@ -32,7 +29,7 @@ def index():
 def login():
 	form = userLogin()
 	if form.validate_on_submit():
-		user = mongo.db.users.find_one({'id': form.handle.data + '@populator.smilodon.social'})
+		user = mongo.db.users.find_one({'id': form.handle.data})
 		if user and User.validate_login(user['password'], form.password.data):
 			user_obj = User(user['id'])
 			login_user(user_obj)
@@ -46,16 +43,15 @@ def returnNewUser(handle, displayName, email, passwordHash):
 	return 	{	
 						'id': handle+'@populator.smilodon.social', 
 						'context': 'https://www.w3.org/ns/activitystreams', 
-						'preferredUsername': displayName, 
 						'name': displayName, 
 						'email': email, 
 						'password': passwordHash, 
 						'type': 'Person', 
-						'following': 'http://populator.smilodon.social/'+handle+'following.json', 
-						'followers': 'http://populator.smilodon.social/'+handle+'followers.json', 
-						'liked': 'http://populator.smilodon.social/'+handle+'liked.json', 
-						'inbox': 'http://populator.smilodon.social/'+handle+'inbox.json', 
-						'outbox': 'http://populator.smilodon.social/'+handle+'feed.json', 
+						'following': 'http://populator.smilodon.social/'+handle+'/following.json', 
+						'followers': 'http://populator.smilodon.social/'+handle+'/followers.json', 
+						'liked': 'http://populator.smilodon.social/'+handle+'/liked.json', 
+						'inbox': 'http://populator.smilodon.social/'+handle+'/inbox.json', 
+						'outbox': 'http://populator.smilodon.social/'+handle+'/feed.json', 
 						'summary': '', 
 						'icon': ''
 					}
@@ -78,13 +74,59 @@ def register():
 			return render_template('registration.html', form=form)
 	return render_template('registration.html', form=form)
 
+def createPost(text, name, id, receivers):
+	return {
+						"@type": "Create",
+						"actor": {
+												"@type": "Person",
+												"@id": id,
+												"@displayName": name
+											},
+						"to": receivers,
+						"object": {
+							"@type": "Note",
+							"content": text
+						}
+					}
 
 @app.route('/compose', methods=['GET', 'POST'])
 @login_required
 def compose():
 	form = composePost()
 	if form.validate_on_submit():
-		putData = {'text': form.text.data, 'poster': current_user.handle}
-		mongo.db.posts.insert_one(putData)
+		post_this = vocab.Create(
+			actor=vocab.Person(
+				current_user.get_id(),
+				displayName=mongo.db.users.find_one({'id': current_user.get_id()})['name']),
+			to=['http://populator.smilodon.social/'+current_user.get_id()+'/feed'],
+			object=vocab.Note(
+				content=form.text.data))
+		mongo.db.posts.insert_one(post_this.json())
 		return redirect(url_for('index'))
 	return render_template('compose.html', form=form)
+
+
+
+
+# user routes
+@app.route('/<handle>/following')
+def following(handle):
+	return mongo.db.following.find({'id': handle+'@populator.smilodon.social'})
+
+@app.route('/<handle>/followers')
+def followers(handle):
+	return mongo.db.followers.find({'id': handle+'@populator.smilodon.social'})
+
+@app.route('/<handle>/liked')
+def liked(handle):
+	return mongo.db.liked.find({'id': handle+'@populator.smilodon.social'})
+
+@app.route('/<handle>/inbox')
+def inbox(handle):
+	return mongo.db.inbox.find({'id': handle+'@populator.smilodon.social'})
+
+@app.route('/<handle>/feed', methods=["POST"])
+def postToFeed(handle, post):
+	mongo.db.posts.insert(post, ordered=True)
+	
+
