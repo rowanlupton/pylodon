072f53460fbf64011b5a2c10ebb0e4428df278d9
rowan <rowanlupton@gmail.com>
2017-09-16 15:57:30 -0400

    fixed double-nested to arrays

--- app/utilities.py
+++ app/utilities.py
@@ -2,6 +2,7 @@ from app import mongo
 
 from activipy import vocab
 from flask import request, abort
+from flask_login import current_user
 
 def find_user_or_404(handle):
   u = mongo.db.users.find_one({'id': request.url_root + handle})
@@ -23,11 +24,7 @@ def check_headers(request):
   print(request.headers)
   if request.headers.get('Content-Type'):
     if (request.headers['Content-Type'] == 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"') or (request.headers['Content-Type'] == 'application/activity+json'):
-      print(request.headers)
       return True
-    else:
-      print(request.headers)
-  print(request.headers)
   return False
 
 def createPost(text, name, id, receivers):
@@ -35,6 +32,6 @@ def createPost(text, name, id, receivers):
                       actor=vocab.Person(
                             id,
                             displayName=name),
-                      to=[receivers],
+                      to=receivers,
                       object=vocab.Note(
                                         content=text))
