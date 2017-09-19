3f173030e00ade770fb1f5709ba99d82642ac318
rowan <rowanlupton@gmail.com>
2017-09-18 21:30:29 -0400

    certbot verify

--- app/views.py
+++ app/views.py
@@ -12,9 +12,9 @@ import requests, json
 app.register_blueprint(api.api)
 
 
-@app.route('/.well-known/acme-challenge/TvLCo6fCY_5gKEqpJi--aK3-Xj-1_g9hf95R6evsQTg.b_4kFtxVpD_AhZGESaoiEWQv0w10su9I7MMJ_qGIGWg')
+@app.route('/.well-known/acme-challenge/RxF73w3YsCwqe6wBlVY_dzZitTpwyVyBgsQXWjTHc8Y.b_4kFtxVpD_AhZGESaoiEWQv0w10su9I7MMJ_qGIGWg')
 def acme_challenge():
-  return app.send_static_file('TvLCo6fCY_5gKEqpJi--aK3-Xj-1_g9hf95R6evsQTg.b_4kFtxVpD_AhZGESaoiEWQv0w10su9I7MMJ_qGIGWg')
+  return app.send_static_file('RxF73w3YsCwqe6wBlVY_dzZitTpwyVyBgsQXWjTHc8Y.b_4kFtxVpD_AhZGESaoiEWQv0w10su9I7MMJ_qGIGWg')
 
 ###################### SET-UP ######################
 @lm.user_loader
