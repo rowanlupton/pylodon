58cf349ec24c449ad84412ed9886ef41154d7ff6
rowan <rowanlupton@gmail.com>
2017-09-21 13:03:13 -0400

    federation is hard

--- app/api.py
+++ app/api.py
@@ -1,5 +1,5 @@
 from app import mongo, rest_api
-from .config import API_HEADERS
+from config import API_HEADERS
 from .utilities import find_user_or_404, get_logged_in_user, check_headers, createPost, get_time, follow_user, accept_follow
 
 from flask import Blueprint, request, abort, redirect, url_for
