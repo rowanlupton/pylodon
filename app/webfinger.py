33fd4c97cc65f6d10d6b957ede9921c967a8c119
rowan <rowanlupton@gmail.com>
2017-09-21 14:55:05 -0400

    federation is hard

--- app/webfinger.py
+++ app/webfinger.py
@@ -9,7 +9,7 @@ webfinger = Blueprint('webfinger', __name__, template_folder='templates')
 def get_user_info(**kwargs):
   id = unquote(request.args['resource'])
   print(id)
-  if request.args['rel']:
+  if rel in request.args:
     rel = request.args['rel']
   u = mongo.db.users.find_one({'id': id})
   print(u.json())
