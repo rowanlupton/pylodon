016a77b168e17423546c8916085cb40fdb93e3ab
rowan <rowanlupton@gmail.com>
2017-09-21 14:42:55 -0400

    federation is hard

--- app/webfinger.py
+++ app/webfinger.py
@@ -13,34 +13,34 @@ def get_user_info(**kwargs):
   u = mongo.db.users.find_one({'id': id})
 
   # if rel is not 'me':
-    resp = {
-            'subject': u['id'],
-            'aliases': [
-              request.url_root+'@'+u['username'],
-              request.url_root+'users/'+u['username']
-            ],
-            'properties': {
-              'http://schema.org/name': u['name']
+  resp = {
+          'subject': u['id'],
+          'aliases': [
+            request.url_root+'@'+u['username'],
+            request.url_root+'users/'+u['username']
+          ],
+          'properties': {
+            'http://schema.org/name': u['name']
+          },
+          'links': [
+            {
+              'rel': 'http://webfinger.net/rel/profile-page',
+              'href': request.url_root+'@'+u['username']
             },
-            'links': [
-              {
-                'rel': 'http://webfinger.net/rel/profile-page',
-                'href': request.url_root+'@'+u['username']
-              },
-              {
-                'href': request.url_root+'api/'+u['username'],
-                'rel': 'self',
-                'type': 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
-              }
-            ]
-          }
-    resp = {
-            'subject': request.url_root+u['username'],
-            'aliases': [u['acct']],
-            'links': [
-                      {'rel': 'self', 'type': "application/ld+json; profile=\"https://www.w3.org/ns/activitystreams\"", 'href': request.url_root+u['username']}
-                      ]
-    }
+            {
+              'href': request.url_root+'api/'+u['username'],
+              'rel': 'self',
+              'type': 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+            }
+          ]
+        }
+    # resp = {
+    #         'subject': request.url_root+u['username'],
+    #         'aliases': [u['acct']],
+    #         'links': [
+    #                   {'rel': 'self', 'type': "application/ld+json; profile=\"https://www.w3.org/ns/activitystreams\"", 'href': request.url_root+u['username']}
+    #                   ]
+    # }
 
     return jsonify(resp)
 
