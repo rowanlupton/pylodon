4c72fe0f09e5226a9c2b5cc4db09bdba63e6858f
rowan <rowanlupton@gmail.com>
2017-09-26 14:00:50 -0400

    find the inbox before trying to send to it

--- app/api.py
+++ app/api.py
@@ -72,7 +72,8 @@ class inbox(Resource):
         print(r)
         mongo.db.users.update_one({'id': u['id']}, {'$push': {'followers_coll': r['actor']}}, upsert=True)
         accept = createAccept(r)
-        requests.post(accept['to'], data=accept, headers=API_CONTENT_HEADERS)
+        to = requests.get(accept['to'], headers=API_ACCEPT_HEADERS)['inbox']
+        requests.post(to, data=accept, headers=API_CONTENT_HEADERS)
 
       if r['type'] == 'Accept':
         mongo.db.users.update_one({'id': u['id']}, {'$push': {'following_coll': r['actor']}}, upsert=True)
