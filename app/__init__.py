90fedd765f53b4959072d5f891c7d407d9f93dbe
rowan <rowanlupton@gmail.com>
2017-11-21 09:49:17 -0500

    yet a different logging attempt

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,9 @@
-from flask import Flask
+from flask import Flask, logging
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_pymongo import PyMongo
 from flask_restful import Api
 
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
 app = Flask(__name__)
 app.config.from_object('config')
