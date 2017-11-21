f2d07edfa14733f5c521d09ce977201257549059
rowan <rowanlupton@gmail.com>
2017-11-21 09:41:57 -0500

    changed logging strategy (bc the previous one didn't work)

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,24 @@ from flask_mail import Mail
 from flask_pymongo import PyMongo
 from flask_restful import Api
 
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
 app.config.from_object('config')
 lm = LoginManager()
