14499a7d8a33cd87441af5fb090325d5e51b0757
rowan <rowanlupton@gmail.com>
2017-11-22 18:32:05 -0500

    get object actor, instead of just the whole object

--- app/api.py
+++ app/api.py
@@ -74,7 +74,7 @@ class inbox(Resource):
           if r['actor'] in u['followers_coll']:
             return 400
         mongo.db.users.update_one({'id': u['id']}, {'$push': {'followers_coll': r['actor']}}, upsert=True)
-        to = requests.get(r['object'], headers=sign_headers(u, API_ACCEPT_HEADERS)).json()['inbox']
+        to = requests.get(r['object']['actor'], headers=sign_headers(u, API_ACCEPT_HEADERS)).json()['inbox']
         accept = createAccept(r, to)
         sign_object(u, accept)
         headers = sign_headers(u, API_CONTENT_HEADERS)
