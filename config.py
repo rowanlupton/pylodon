96b5f51ab28cc9e53ac1f2b285dabab009184d19
rowan <rowanlupton@gmail.com>
2017-11-23 21:30:46 -0500

    fixed a dict declaration

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DB_NAME = 'Cluster0'
 MONGO_URI = 'mongodb://rowanlupton:'+os.environ['mongodb-password']+'@cluster0-shard-00-00-jjly0.mongodb.net:27017,cluster0-shard-00-01-jjly0.mongodb.net:27017,cluster0-shard-00-02-jjly0.mongodb.net:27017/smilodon?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 MONGO_DBNAME = 'Cluster0'
 
-API_CONTENT_HEADERS = dict(Content-Type: application/ld+json; profile="https://www.w3.org/ns/activitystreams")
+API_CONTENT_HEADERS = dict(Content-Type = application/ld+json; profile="https://www.w3.org/ns/activitystreams")
 API_ACCEPT_HEADERS = dict(Accept = application/ld+json; profile="https://www.w3.org/ns/activitystreams")
 # specified by ActivityPub/ActivityStreams
 VALID_HEADERS = ( 
