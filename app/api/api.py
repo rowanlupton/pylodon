af4dfa248d7ee5d96521109f8cb753541060370c
rowan <rowanlupton@gmail.com>
2017-11-26 12:40:53 -0500

    important to import config variables before using them

--- app/api/api.py
+++ app/api/api.py
@@ -1,7 +1,7 @@
 # thanks to https://github.com/snarfed for the authorization -> signature headers hack
 
 from app import mongo, rest_api
-from config import API_ACCEPT_HEADERS, API_CONTENT_HEADERS
+from config import API_ACCEPT_HEADERS, API_CONTENT_HEADERS, DEFAULT_CONTEXT
 from .users import User
 from .utilities import check_accept_headers, check_content_headers, find_user_or_404, get_address_from_webfinger, get_time, return_new_user, sign_headers, sign_object
 
