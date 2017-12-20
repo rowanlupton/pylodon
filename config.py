5cf68504ddec712c45b26f79af1ba5d7c71f0126
rowan <rowanlupton@gmail.com>
2017-12-20 01:25:14 -0500

    switched to putting the entire mongodb uri as an environemnt variable

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = os.environ['secret_key']
 DB_NAME = 'Cluster0'
 
-MONGO_URI = 'mongodb://rowanlupton:'+os.environ['mongodb-password']+'@cluster0-shard-00-00-jjly0.mongodb.net:27017,cluster0-shard-00-01-jjly0.mongodb.net:27017,cluster0-shard-00-02-jjly0.mongodb.net:27017/smilodon?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+MONGO_URI = os.environ['mongodb_uri']
 MONGO_DBNAME = 'Cluster0'
 
 valid_header = 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
