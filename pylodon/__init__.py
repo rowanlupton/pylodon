02a3a1986afd67c79e60a8f28f800535a22a05ca
rowan <rowanlupton@gmail.com>
2017-12-23 16:15:17 -0500

    added flask-cors

--- pylodon/__init__.py
+++ pylodon/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from flask_sslify import SSLify
 
 from config import STRICT_HTTPS
@@ -7,6 +8,7 @@ from config import STRICT_HTTPS
 app = Flask(__name__)
 app.config.from_object('config')
 
+CORS(app)
 mongo = PyMongo(app)
 if STRICT_HTTPS:
     sslify = SSLify(app, subdomains=True, permanent=True)
